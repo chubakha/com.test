6a343be0bd30b31ce9ddbeab59ad436e6fb0ffec
chubaha <you@example.com>
2022-05-07 20:36:54 +0300

    second commit

--- src/test/java/com/test/BaseTest.java
+++ src/test/java/com/test/BaseTest.java
@@ -16,15 +16,15 @@ public abstract class BaseTest {
     static final String REGISTRATION_COMPANY_NAME = "Chubsung";
     static final String REGISTRATION_FIRST_NAME = "Alex";
     static final String REGISTRATION_LAST_NAME = "Chu";
-    static final String REGISTRATION_PASSWORD1 = "12345678";
-    static final String REGISTRATION_PASSWORD2 = "12345678";
+    static final String REGISTRATION_PASSWORD1 = "********";
+    static final String REGISTRATION_PASSWORD2 = "********";
 
     static final String STABLE_CLIENT_EMAIL = "client-yop@yopmail.com";
-    static final String STABLE_CLIENT_PASSWORD = "12345678";
+    static final String STABLE_CLIENT_PASSWORD = "********";
     static final String STABLE_MANAGER_EMAIL = "man-yop@yopmail.com";
-    static final String STABLE_MANAGER_PASSWORD = "12345678";
+    static final String STABLE_MANAGER_PASSWORD = "********";
     static final String STABLE_PROVIDER_EMAIL = "prov-yop@yopmail.com";
-    static final String STABLE_PROVIDER_PASSWORD = "12345678";
+    static final String STABLE_PROVIDER_PASSWORD = "********";
 
 
 }
