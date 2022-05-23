2665bafbd6ec9c63b51bda93fcb42f62e8450a56
chubaha <you@example.com>
2022-05-23 19:00:32 +0300

    verify active and inactivate "I’ve read and accepted Terms & Conditions and Privacy Policy." checkbox

--- src/test/java/com/test/registration/fourth_registration_page/VerifyInactivateIveReadAndAcceptedTermsConditionsAndPrivacyPolicyTest.java
+++ src/test/java/com/test/registration/fourth_registration_page/VerifyInactivateIveReadAndAcceptedTermsConditionsAndPrivacyPolicyTest.java
@@ -1,5 +1,6 @@
 package com.test.registration.fourth_registration_page;
 
+import com.test.registration.PrepareRegistrationTestData;
 import com.test.registration.first_registration_page.FirstRegistrationPage;
 import com.test.registration.second_registration_page.SecondRegistrationPage;
 import com.test.registration.third_page_registration.ThirdRegistrationPage;
@@ -8,7 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.sleep;
 
-public class VerifyInactivateIveReadAndAcceptedTermsConditionsAndPrivacyPolicyTest {
+public class VerifyInactivateIveReadAndAcceptedTermsConditionsAndPrivacyPolicyTest extends PrepareRegistrationTestData {
     @Test
     void verifyInactivateIveReadAndAcceptedTermsConditionsAndPrivacyPolicy() {
         new FirstRegistrationPage()
@@ -22,6 +23,6 @@ public class VerifyInactivateIveReadAndAcceptedTermsConditionsAndPrivacyPolicyTe
         FourthRegistrationPage fourthRegistrationPage = new FourthRegistrationPage()
                 .clickIveReadAndAcceptedTermsConditionsAndPrivacyPolicy()
                 .clickIveReadAndAcceptedTermsConditionsAndPrivacyPolicy();
-        Assertions.assertEquals(true, fourthRegistrationPage.getDisabledCreateAccountButton(), "'CREATE ACCOUNT' button shouldn't be with attribute 'disabled'");
+        Assertions.assertEquals("true", fourthRegistrationPage.getDisabledCreateAccountButton(), "'CREATE ACCOUNT' button shouldn't be with attribute 'disabled'");
     }
 }
